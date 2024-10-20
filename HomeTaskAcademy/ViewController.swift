cff27f86be73bec98ec22721896c7d6965a7f22b
uladislav-st <uladislav.st@gmail.com>
2024-10-21 02:14:49 +0300

    Initial Commit

---
+++ HomeTaskAcademy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTaskAcademy
+//
+//  Created by Vlad Stankevich on 21.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
