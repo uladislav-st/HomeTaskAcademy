de1ee43c0cce5240a5a1e97d7fbf9015bc7f29b6
uladislav-st <uladislav.st@gmail.com>
2024-11-04 15:34:49 +0300

    working with viewControlPanel

--- HomeTaskAcademy/ViewController.swift
+++ HomeTaskAcademy/ViewController.swift
@@ -1,12 +1,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let myView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    @IBAction func addViewButton(_ sender: UIButton) {
+        addView()
+    }
+    @IBAction func movieViewButton(_ sender: UIButton) {
+        myView.frame.origin.y += CGFloat(Int.random(in: 0...200))
+    }
+    
+    func addView( ){
+        myView.frame = CGRect(
+            x: Int.random(in: 0...400),
+            y: Int.random(in: 0...400),
+            width: 100,
+            height: 100)
+        myView.backgroundColor = .cyan
+        view.addSubview(myView)
+    }
 
 }
 
