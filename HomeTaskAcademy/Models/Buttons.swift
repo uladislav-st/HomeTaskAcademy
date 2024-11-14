35c47ab832fecbf2ab86dc125f187c0397cff748
uladislav-st <uladislav.st@gmail.com>
2024-11-14 18:36:35 +0300

    new folder Models

---
+++ HomeTaskAcademy/Models/Buttons.swift
@@ -0,0 +1,29 @@
+//
+//  Buttons.swift
+//  HomeTaskAcademy
+//
+//  Created by Vlad Stankevich on 14.11.24.
+//
+
+import Foundation
+import UIKit
+
+
+final class Buttons {
+    
+    static  let createSquareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("CREATE", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    static let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("CLEAR", for: .normal)
+        button.backgroundColor = .systemRed
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+}
