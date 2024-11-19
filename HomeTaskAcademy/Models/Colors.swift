3dac626a574be65674423d95f106c3e70ec8b3bc
uladislav-st <uladislav.st@gmail.com>
2024-11-19 14:38:12 +0300

    reverse engineering

---
+++ HomeTaskAcademy/Models/Colors.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static let customBlue = UIColor.systemBlue
+    static let customBackground = UIColor.white
+}
+
---
+++ HomeTaskAcademy/Models/Models.swift
@@ -0,0 +1,5 @@
+struct Product {
+    let name: String
+    var quantity: Int
+    let price: Int
+}
