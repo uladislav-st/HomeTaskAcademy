f228aabbcd38ad44dd95d262f60aa6e1cf7916be
uladislav-st <uladislav.st@gmail.com>
2024-11-20 01:47:45 +0300

    constraints

--- HomeTaskAcademy/SecondViewController.swift
+++ HomeTaskAcademy/SecondViewController.swift
@@ -1,1 +1,48 @@
 
+import UIKit
+import Foundation
+import SwiftUI
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+        
+        let label = UILabel()
+        label.text = "This is the second screen!"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isUserInteractionEnabled = true
+
+        
+        view.addSubview(label)
+        
+        let backButton = UIButton(type: .system)
+        backButton.setTitle("Go Back", for: .normal)
+        backButton.backgroundColor = .green
+        backButton.isUserInteractionEnabled = true
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.addTarget(self, action: #selector(dismissScreen), for: .touchUpInside)
+        
+        view.addSubview(backButton)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50),
+            label.heightAnchor.constraint(equalToConstant: 40),
+            
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            backButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            backButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50),
+            backButton.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+    }
+    
+    @objc private func dismissScreen() {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- HomeTaskAcademy/ViewController.swift
+++ HomeTaskAcademy/ViewController.swift
@@ -1,12 +1,63 @@
 import UIKit
+import Foundation
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
+    private let textLabelFirst: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.isUserInteractionEnabled = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .cyan
+        return label
+    }()
+    
+    private let textLabelSecond: UILabel = {
+        let label = UILabel()
+        label.text = "Go next!"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.isUserInteractionEnabled = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .black
+        
+        view.addSubview(textLabelFirst)
+        view.addSubview(textLabelSecond)
+        
+        NSLayoutConstraint.activate([
+            textLabelFirst.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            textLabelFirst.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            textLabelFirst.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            textLabelFirst.heightAnchor.constraint(equalToConstant: 50),
+            
+            textLabelSecond.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            textLabelSecond.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            textLabelSecond.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)
+        ])
+        
+        let firstLabelTap = UITapGestureRecognizer(target: self, action: #selector(handleFirstLabelTap))
+        let secondLabelTap = UITapGestureRecognizer(target: self, action: #selector(handleSecondLabelTap))
+        
+        textLabelFirst.addGestureRecognizer(firstLabelTap)
+        textLabelSecond.addGestureRecognizer(secondLabelTap)
     }
 
-
+    
+    @objc private func handleFirstLabelTap() {
+        textLabelFirst.text = (textLabelFirst.text == "Hello") ? "Hi!" : "Hello"
+    }
+    
+    @objc private func handleSecondLabelTap() {
+        let secondViewController = SecondViewController()
+        present(secondViewController, animated: true, completion: nil)
+    }
 }
-
